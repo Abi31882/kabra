3a7857d6a8567f8a2df1048e0f577e90870fe874
Abi chaudhary <abhinavchaudhary348@gmail.com>
2022-03-19 22:25:39 +0530

    added the cors

--- Controllers/productcontroller.js
+++ Controllers/productcontroller.js
@@ -37,7 +37,8 @@ exports.addProduct = async (req, res, next) => {
 exports.getAllProducts = async (req, res) => {
   try {
     const doc = await Product.find();
-    res.status(200).json(doc);
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Origin", "*").status(200).json(doc);
   } catch (e) {
     res.json(e);
   }
