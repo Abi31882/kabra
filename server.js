f02fe55989cd079b32e5af814646582c618e15cf
Abi chaudhary <abhinavchaudhary348@gmail.com>
2022-03-19 23:00:24 +0530

    added the cors

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-process.on("uncaughtException", (err) => {
-  console.log("UNCAUGHT EXCEPTION! SHUTTING DOWN");
-  console.log(err.name, err.message);
-  process.exit(1);
-});
+// process.on("uncaughtException", (err) => {
+//   console.log("UNCAUGHT EXCEPTION! SHUTTING DOWN");
+//   console.log(err.name, err.message);
+//   process.exit(1);
+// });
 
 const app = require("./app");
 dotenv.config({ path: "./config.env" });
