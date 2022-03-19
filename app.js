a89194060965c67480b398347c1bf8e1d428c2b0
Abi chaudhary <abhinavchaudhary348@gmail.com>
2022-03-19 22:17:23 +0530

    added the cors

--- app.js
+++ app.js
@@ -8,12 +8,32 @@ var fileupload = require("express-fileupload");
 
 app.use(fileupload());
 
-app.use(express.json());
-const corsOptions = {
-  origin: "https://ecstatic-euler-b704c3.netlify.app/",
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://ecstatic-euler-b704c3.netlify.app"
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 // app.use(cors());
 // app.options("*", cors());
 
