560e1f85e025e4139269d1d66a0a1a270a3261c5
Abi chaudhary <abhinavchaudhary348@gmail.com>
2022-03-20 09:20:56 +0530

    tried

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const ProductController = require("../Controllers/productcontroller.js");
+const ProductController = require("../Controllers/productcontroller");
 const cartRouter = require("./cartRoutes");
 
 const router = express.Router({ mergeParams: true });
 router.use("/:productId/cart/:cartId", cartRouter);
 
-router.post("/create", ProductController.addProduct);
-router.get("/", ProductController.getAllProducts);
+router.post("/create", ProductController.addProduct());
+router.get("/", ProductController.getAllProducts());
 
 module.exports = router;
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const userController = require("../Controllers/cartController");
 const router = express.Router();
 
-router.route("/signup").post(userController.singup);
-router.route("/login").post(userController.login);
+router.route("/signup").post(userController.singup());
+router.route("/login").post(userController.login());
 router.get(
   "/me",
-  userController.protect,
-  userController.getMe,
-  userController.getUser
+  userController.protect(),
+  userController.getMe(),
+  userController.getUser()
 );
 module.exports = router;
